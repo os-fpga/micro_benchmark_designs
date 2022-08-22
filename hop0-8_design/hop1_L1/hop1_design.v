7fe936771c92a1fffd3c569b08637e48801f4e67
Roman Shah <rshah@interactivenode3.rapid.local>
2022-08-22 13:44:28 +0500

    sub folder added for all designs

---
+++ hop0-8_design/hop1_L1/hop1_design.v
@@ -0,0 +1,27 @@
+module hop1_design( clock0, rst,ff2,start);
+
+   	input clock0, rst,start;
+   	output ff2;
+
+   reg ff1,ff2;
+
+
+  	always @( posedge clock0, posedge rst )
+   	begin
+   		if( rst )
+       			ff1 <= 0;
+		else
+			ff1<= start;
+	end
+
+
+	always @(posedge clock0)
+	begin
+    		if( rst )
+       			ff2 <= 0;      
+    		else 
+			ff2<= ff1;
+
+	end
+
+endmodule
